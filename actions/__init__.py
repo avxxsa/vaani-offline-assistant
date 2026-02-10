e7fc8633a5430940e9efb6546f76d33f5397bdce
Suprabha Bhuju <suprabhaa19@gmail.com>
2026-02-11 00:52:58 +0545

    fix: Stop tracking unnecessary files (venv, models, logs)

---
+++ actions/__init__.py
@@ -0,0 +1,15 @@
+from .general_actions import greeting, time_skill, fallback_skill
+from .reminder_actions import set_reminder, get_reminders, delete_reminder
+from .todo_actions import add_todo, get_todos, delete_todo
+from .journal_actions import add_journal, get_journals, delete_journal_by_index
+from .system_actions import open_application, get_platform, get_system_info
+from .search_actions import search_in_common_locations, local_file_search, web_search
+
+__all__ = [
+    'greeting', 'time_skill', 'fallback_skill',
+    'set_reminder', 'get_reminders', 'delete_reminder',
+    'add_todo', 'get_todos', 'delete_todo',
+    'add_journal', 'get_journals', 'delete_journal_by_index',
+    'open_application', 'get_platform', 'get_system_info',
+    'search_in_common_locations', 'local_file_search', 'web_search'
+]
